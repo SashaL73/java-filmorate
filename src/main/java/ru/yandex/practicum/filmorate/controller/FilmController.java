bf56bb899595aa293c22ea4ceb9e43f6fee5e740
Lebedenko Aleksander <lebedenko.aleksander2015@yandex.ru>
2026-01-11 17:15:01 +0300

    Созданы контроллеры согласно ТЗ

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,90 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+
+    private final Map<Long, Film> films = new HashMap<>();
+
+    @GetMapping
+    public List<Film> getFilms() {
+        return new ArrayList<>(films.values());
+    }
+
+    @PostMapping
+    public Film addFilm(@Valid @RequestBody Film film) {
+
+        try {
+            film.setId(getNextId());
+            films.put(film.getId(), film);
+            log.info("Добавлен фильм: {}", film);
+            return film;
+        } catch (ConstraintViolationException e) {
+            log.error(e.getMessage());
+            return null;
+        }
+
+    }
+
+    @PutMapping
+    public Film updateFilm(@Valid @RequestBody Film film) {
+
+        try {
+
+            if (!films.containsKey(film.getId())) {
+                String errorMessage = "Такого фильма нет";
+                log.error(errorMessage);
+                throw new ValidationException(errorMessage);
+            }
+
+            Film updatedFilm = films.get(film.getId());
+
+            if (film.getName() != null) {
+                updatedFilm.setName(film.getName());
+            }
+
+            if (film.getReleaseDate() != null) {
+                updatedFilm.setReleaseDate(film.getReleaseDate());
+            }
+
+            if (film.getDescription() != null) {
+                updatedFilm.setDescription(film.getDescription());
+            }
+
+            if (film.getDuration() != null) {
+                updatedFilm.setDuration(film.getDuration());
+            }
+
+            films.put(film.getId(), updatedFilm);
+            log.info("Фильм изменен: {}", film);
+            return updatedFilm;
+
+        } catch (ConstraintViolationException e) {
+            log.error(e.getMessage());
+            return null;
+        }
+
+    }
+
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
 }
