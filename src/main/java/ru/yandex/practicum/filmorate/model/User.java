3bc490505287684e038e7767b1f3657648c2e18f
Lebedenko Aleksander <lebedenko.aleksander2015@yandex.ru>
2026-01-11 18:10:32 +0300

    Добавлены свойства согласно тз

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDate;
+
+@Data
+@Slf4j
+public class User {
+
+    private Long id;
+    @Email(message = "Некорректный формат email")
+    @NotEmpty(message = "Имейл не должен быть пустым")
+    private String email;
+    @NotEmpty(message = "Логин не может быть пустым")
+    @Pattern(regexp = "^\\S+$", message = "Логин не должно содержать пробелов")
+    private String login;
+    private String name;
+    @Past(message = "Дата рождения не может быть в будущем")
+    private LocalDate birthday;
+
+}
