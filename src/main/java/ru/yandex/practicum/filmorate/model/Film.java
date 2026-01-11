f178bc6a2b5d46bba54af81a5cd65d60c8e827cb
Lebedenko Aleksander <lebedenko.aleksander2015@yandex.ru>
2026-01-11 17:15:25 +0300

    Добавлены свойства согласно тз

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,25 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import ru.yandex.practicum.filmorate.validate.MinimumDate;
+
+import java.time.LocalDate;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
 public class Film {
+
+    private Long id;
+    @NotEmpty(message = "Название должно быть указано")
+    private String name;
+    @Size(max = 200, message = "Максимальная длинна описания 200 символов")
+    private String description;
+    @MinimumDate
+    private LocalDate releaseDate;
+    @Positive(message = "Продолжительность фильма должна быть положительным числом")
+    private Integer duration;
+
 }
