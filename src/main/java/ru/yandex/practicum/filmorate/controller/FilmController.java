49ab456f275ae1076a399578d3bd48d43a83f393
Lebedenko Aleksander <lebedenko.aleksander2015@yandex.ru>
2026-01-30 23:58:08 +0300

    Переделан код контроллера

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,82 +2,84 @@ package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.ActionType;
+import ru.yandex.practicum.filmorate.service.FilmService;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RestController
 @RequestMapping("/films")
 public class FilmController {
 
-    private final Map<Long, Film> films = new HashMap<>();
+    private final FilmService filmService;
+
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
+
 
     @GetMapping
     public List<Film> getFilms() {
-        return new ArrayList<>(films.values());
+        return filmService.getFilms();
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Film addFilm(@Valid @RequestBody Film film) {
-
-        film.setId(getNextId());
-        films.put(film.getId(), film);
-        log.info("Добавлен фильм: {}", film);
-        return film;
-
+        return filmService.addFilm(film);
     }
 
     @PutMapping
     public Film updateFilm(@Valid @RequestBody Film film) {
-
         if (film.getId() == null) {
             String errorMessage = "Id не должен быть пустым";
             log.error(errorMessage);
             throw new ValidationException(errorMessage);
         }
 
-        if (!films.containsKey(film.getId())) {
-            String errorMessage = "Такого фильма нет";
-            log.error(errorMessage);
-            throw new ValidationException(errorMessage);
-        }
-
-        Film updatedFilm = films.get(film.getId());
-
-        if (film.getName() != null) {
-            updatedFilm.setName(film.getName());
-        }
-
-        if (film.getReleaseDate() != null) {
-            updatedFilm.setReleaseDate(film.getReleaseDate());
-        }
-
-        if (film.getDescription() != null) {
-            updatedFilm.setDescription(film.getDescription());
-        }
-
-        if (film.getDuration() != null) {
-            updatedFilm.setDuration(film.getDuration());
-        }
-
-        films.put(film.getId(), updatedFilm);
-        log.info("Фильм изменен: {}", film);
-        return updatedFilm;
+        return filmService.updateFilm(film);
 
     }
 
-    private long getNextId() {
-        long currentMaxId = films.keySet()
-                .stream()
-                .mapToLong(id -> id)
-                .max()
-                .orElse(0);
-        return ++currentMaxId;
+    @PutMapping("/{id}/like/{userId}")
+    public boolean addLikeFilm(@PathVariable("id") Long id,
+                               @PathVariable("userId") Long userId) {
+        if (id < 0 || id == null) {
+            throw new ValidationException("Некорректный Id");
+        }
+
+        if (userId < 0 || userId == null) {
+            throw new ValidationException("Некорректный Id");
+        }
+
+        return filmService.updatedLikeFilm(id, userId, ActionType.ADD);
     }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public boolean deleteLikeFilm(@PathVariable("id") Long id,
+                                  @PathVariable("userId") Long userId) {
+        if (id < 0 || id == null) {
+            throw new ValidationException("Некорректный Id");
+        }
+
+        if (userId < 0 || userId == null) {
+            throw new ValidationException("Некорректный Id");
+        }
+
+        return filmService.updatedLikeFilm(id, userId, ActionType.DELETE);
+    }
+
+    @GetMapping("/popular")
+    public List<Film> getPopularFilms(@RequestParam(defaultValue = "10", name = "count") int count) {
+        if (count < 0) {
+            throw new ValidationException("count должен быть больше нуля");
+        }
+        return filmService.getMostPopularFilms(count);
+    }
+
 }
