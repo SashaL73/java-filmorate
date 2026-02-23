bde1cd8c076caafcac03c8217726ca25ed55699f
Lebedenko_AE <lebedenko.aleksander2015@yandex.ru>
2026-02-23 14:07:21 +0300

    изменил rowMap так как теперь Film собирается одним SQL запросом

--- src/main/java/ru/yandex/practicum/filmorate/storage/dal/mappers/FilmRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dal/mappers/FilmRowMapper.java
@@ -3,10 +3,14 @@ package ru.yandex.practicum.filmorate.storage.dal.mappers;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
 
 @Component
 public class FilmRowMapper implements RowMapper<Film> {
@@ -23,7 +27,25 @@ public class FilmRowMapper implements RowMapper<Film> {
         mpa.setId(resultSet.getInt("mpa_id"));
         mpa.setName(resultSet.getString("mpa"));
         film.setMpa(mpa);
+        LinkedHashSet<Genre> genreLinkedHashSet = new LinkedHashSet<>();
+        if (resultSet.getString("genres") != null) {
+            String genres = resultSet.getString("genres");
+            String[] genreArray = genres.split(",");
+            String genresIds = resultSet.getString("genre_ids");
+            String[] idArray = genresIds.split(",");
+            long[] genresId = new long[idArray.length];
+            for (int i = 0; i < idArray.length; i++) {
+                genresId[i] = Long.parseLong(idArray[i]);
+            }
+            for (int i = 0; i < genreArray.length; i++) {
+                Genre genre = new Genre();
+                genre.setId(genresId[i]);
+                genre.setName(genreArray[i]);
+                genreLinkedHashSet.add(genre);
+            }
+        }
+        List<Genre> genreList = new ArrayList<>(genreLinkedHashSet);
+        film.setGenres(genreList);
         return film;
-
     }
 }
